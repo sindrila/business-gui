da8652214c1ef39d4d7455963923915a6d20ceda
Mindrila Alex <mihail.mindrila@stud.ubbcluj.ro>
2024-04-10 11:49:01 +0300

    Removed errors

--- MVVM/Model/Post.cs
+++ MVVM/Model/Post.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+
 public class Post
 {
     private int _id;
@@ -8,6 +9,7 @@ public class Post
     private string _imagePath;
     private string _caption;
     private List<Comment> _comments;
+    //private Product _product;
 
     public int Id => _id;
     public int BusinessId => _businessId;
@@ -16,8 +18,10 @@ public class Post
     public string ImagePath => _imagePath;
     public string Caption => _caption;
     public List<Comment> Comments => _comments;
+    //public Product Product => _product;
 
     public Post(int id, int businessId, DateTime creationDate, string imagePath, string caption)
+    //public Post(int id, int businessId, DateTime creationDate, string imagePath, string caption, Product product)
 
     {
         _id = id;
@@ -27,12 +31,15 @@ public class Post
         _imagePath = imagePath;
         _caption = caption;
         _comments = new List<Comment>();
+        //_product = product;
     }
 
+    public void SetBusinessId(int businessId) => _businessId = businessId;
     public void SetNumberOfLikes(int likes) => _numberOfLikes = likes;
     public void SetCreationDate(DateTime creationDate) => _creationDate = creationDate;
     public void SetImagePath(string imagePath) => _imagePath = imagePath;
     public void SetCaption(string caption) => _caption = caption;
+    public void SetComments(List<Comment> comments) => _comments = comments;
     //public void SetProduct(Product product) => _product = product;
 
     public void AddLike() { _numberOfLikes++; }
