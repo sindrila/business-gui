eed00f10b4aa89f5df7a0b39ce12bb1d7befb375
Nistor Bianca <bianca.nistor55@gmail.com>
2024-04-10 12:22:15 +0300

    Add Logo and Add Banner buttons
    Works, modified the CreateBusiness.xaml, and in ViewModel the CreateBusinessViewModel.cs

--- MVVM/ViewModel/CreateNewBusinessViewModel.cs
+++ MVVM/ViewModel/CreateNewBusinessViewModel.cs
@@ -5,7 +5,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Windows.Input;
+using Microsoft.Win32;
+using System.Windows;
 namespace bussiness_social_media.MVVM.ViewModel
 {
     public class CreateNewBusinessViewModel : Core.ViewModel
@@ -22,11 +24,28 @@ namespace bussiness_social_media.MVVM.ViewModel
             }
         }
 
+        public ICommand AddLogoCommand { get; private set; }
+        public ICommand AddBannerCommand {  get; private set; }
         public RelayCommand NavigateToHomeViewModelCommand { get; set; }
         public CreateNewBusinessViewModel(INavigationService navigationService)
         {
             NavigationService = navigationService;
-            NavigateToHomeViewModelCommand = new RelayCommand(o=> {  NavigationService.NavigateTo<HomeViewModel>(); }, o => true);
+            NavigateToHomeViewModelCommand = new RelayCommand(o => { NavigationService.NavigateTo<HomeViewModel>(); }, o => true);
+            AddLogoCommand = new RelayCommand(o => { ExecuteAddPicture(); } , o => true);
+            AddBannerCommand = new RelayCommand(o => { ExecuteAddPicture(); }, o => true);
+        }
+        private String ExecuteAddPicture()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files (*.jpg; *.jpeg; *.png;)|*.jpg; *.jpeg; *.png; |All Files (*.*)|*.*";
+            bool? result = openFileDialog.ShowDialog();
+            if (result == true)
+            {
+                string filename = openFileDialog.FileName;
+                MessageBox.Show(filename);
+                return filename;
+            }
+            else { return null; }
         }
     }
 }
