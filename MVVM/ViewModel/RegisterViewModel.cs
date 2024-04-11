f6bc90f2cf62cf1f7a78d7d4e1c5672f2cbdd27f
Nistor Bianca <bianca.nistor55@gmail.com>
2024-04-11 14:52:16 +0300

    Register Page-does nothing atm

---
+++ MVVM/ViewModel/RegisterViewModel.cs
@@ -0,0 +1,35 @@
+﻿using bussiness_social_media.Core;
+using bussiness_social_media.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bussiness_social_media.MVVM.ViewModel
+{
+    public class RegisterViewModel : Core.ViewModel
+    {
+        private INavigationService _navigation;
+
+        public INavigationService Navigation
+        {
+            get => _navigation;
+            set
+            {
+                _navigation = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public RelayCommand NavigateToHomeCommand { get; set; }
+        public RelayCommand NavigateToCreateNewBusinessViewCommand { get; set; }
+        public RegisterViewModel(INavigationService navigationService)
+        {
+            Navigation = navigationService;
+            NavigateToHomeCommand = new RelayCommand(o => { Navigation.NavigateTo<HomeViewModel>(); }, o => true);
+            NavigateToCreateNewBusinessViewCommand = new RelayCommand(o => { Navigation.NavigateTo<CreateNewBusinessViewModel>(); }, o => true);
+            Navigation.NavigateTo<HomeViewModel>();
+        }
+    }
+}
