fc1d27a24462ddd13a6d989ef92d10ad69431af9
915-Mihalachi-Miruna <mirunamihalache54@gmail.com>
2024-04-11 16:52:24 +0300

    added image path for review creation

--- MVVM/ViewModel/BusinessProfileReviewsViewModel.cs
+++ MVVM/ViewModel/BusinessProfileReviewsViewModel.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Media;
+using Microsoft.Win32;
 using System.Windows.Media.Imaging; // Need to include this for ImageSource
 
 namespace bussiness_social_media.MVVM.ViewModel
@@ -16,6 +17,7 @@ namespace bussiness_social_media.MVVM.ViewModel
         private IBusinessService _businessService;
         private Business _currentBusiness;
         private Review _currentReview;
+        private string _imagePath;
 
         // Property to hold the image source of the current business
         private ImageSource _businessImage;
@@ -44,6 +46,15 @@ namespace bussiness_social_media.MVVM.ViewModel
             get { return ChangeCurrentBusiness(); }
         }
 
+        public string ImagePath
+        {
+            get => _imagePath;
+            set
+            {
+                _imagePath = value;
+                OnPropertyChanged();
+            }
+        }
 
         public INavigationService Navigation
         {
@@ -60,6 +71,7 @@ namespace bussiness_social_media.MVVM.ViewModel
         public RelayCommand NavigateToContactCommand { get; set; }
         public RelayCommand NavigateToAboutCommand { get; set; }
         public RelayCommand LeaveReviewCommand { get; set; }
+        public RelayCommand AddImageCommand { get; private set; }
 
         public BusinessProfileReviewsViewModel(INavigationService navigationService, IBusinessService businessService)
         {
@@ -69,6 +81,7 @@ namespace bussiness_social_media.MVVM.ViewModel
             NavigateToReviewsCommand = new RelayCommand(o => { Navigation.NavigateTo<CreateNewBusinessViewModel>(); }, o => true);
             NavigateToContactCommand = new RelayCommand(o => { Navigation.NavigateTo<BusinessProfileContactViewModel>(); }, o => true);
             NavigateToAboutCommand = new RelayCommand(o => { Navigation.NavigateTo<BusinessProfileAboutViewModel>(); }, o => true);
+            AddImageCommand = new RelayCommand(o => { ExecuteAddImage(); }, o => true);
             LeaveReviewCommand = new RelayCommand(o => { LeaveReview(); }, o => true);
             _currentBusiness = ChangeCurrentBusiness();
             ImageSource img = new BitmapImage(new Uri(_currentBusiness.Logo));
@@ -85,6 +98,19 @@ namespace bussiness_social_media.MVVM.ViewModel
         public Business ChangeCurrentBusiness()
         {
             return _businessService.GetBusinessById(_navigation.BusinessId);
+        }
+
+        private void ExecuteAddImage()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files (*.jpg; *.jpeg; *.png;)|*.jpg; *.jpeg; *.png; |All Files (*.*)|*.*";
+            bool? result = openFileDialog.ShowDialog();
+            if (result == true)
+            {
+                string filename = openFileDialog.FileName;
+                ImagePath = filename;
+            }
+
         }
     }
 }
